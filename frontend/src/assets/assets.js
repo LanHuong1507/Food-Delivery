12c420d6bfacc4fa5368bac609065ce218ea7cf4
LanHuong1507 <taejoon1507@gmail.com>
2024-09-04 12:18:06 +0700

    create navbar and header

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,57 @@
+import logo from './logo.jpg'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+import menu_1 from './salad.png'
+import menu_2 from './rolls.png'
+import menu_3 from './dessert.png'
+import menu_4 from './sandwich.png'
+import menu_5 from './meat.png'
+import menu_6 from './pizza.png'
+import menu_7 from './drinks.png'
+import menu_8 from './sushi.png'
+export const assets = {
+    logo,
+    search_icon,
+    basket_icon,
+    menu_1,
+    menu_2,
+    menu_4,
+    menu_3,
+    menu_5,
+    menu_6,
+    menu_7,
+    menu_8
+}
+export const menu_list=[
+    {
+        menu_name:"Salad",
+        menu_image:menu_1
+    },
+    {
+        menu_name:"Rolls",
+        menu_image:menu_2
+    },
+    {
+        menu_name:"Dessert",
+        menu_image:menu_3
+    },
+    {
+        menu_name:"Sandwich",
+        menu_image:menu_4
+    },{
+        menu_name:"Meat",
+        menu_image:menu_5
+    },
+    {
+        menu_name:"Pizza",
+        menu_image:menu_6
+    },
+    {
+        menu_name:"Drink",
+        menu_image:menu_7
+    },
+    {
+        menu_name:"Sushi",
+        menu_image:menu_8
+    }
+]
