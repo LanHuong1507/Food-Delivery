08d45bcd0161069f62108d7a65fe12e4d7b57149
LanHuong1507 <taejoon1507@gmail.com>
2024-09-06 12:52:49 +0700

    create admin folder

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,14 @@
+import add_icon from './add_icon.png'
+import logo from './logo.jpg'
+import order_icon from './order_icom.png'
+import parcel_icon from './parcel_icon.png'
+import profile_image from './profile_image.png'
+import upload_area from './upload_area.png'
+export const assets = {
+    add_icon,
+    logo,
+    order_icon,
+    parcel_icon,
+    profile_image,
+    upload_area
+}
